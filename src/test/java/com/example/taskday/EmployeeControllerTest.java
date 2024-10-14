c8948b2e70615d236d446dad78b609f2e6ee2647
gabrielc <tuladel.123.team2020@hotmail.com>
2024-10-14 09:33:33 -0300

    BUILD FAIL

--- src/test/java/com/example/taskday/EmployeeControllerTest.java
+++ src/test/java/com/example/taskday/EmployeeControllerTest.java
@@ -344,7 +344,7 @@ public class EmployeeControllerTest {
     public void deleteEmployee() throws Exception{
         mockMvc.perform(delete("http://localhost:8080/employees")
                 .header("Authorization", "Bearer " + loginTokenEmployee)
-        ).andExpect(status().isAccepted());
+        ).andExpect(status().isOk());
     }
 
     public String loginEmployee(EmployeeAuthenticationRequestDTO employeeAuthenticationRequestDTO) throws Exception {
