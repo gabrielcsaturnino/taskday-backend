fd9809d64e358b340d5f0e6231ca1d70966bd822
gabrielc <tuladel.123.team2020@hotmail.com>
2024-10-14 09:45:15 -0300

    BUILD SUCCESS

---
+++ frotend/scripts.js
@@ -0,0 +1,10 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    const userType = document.getElementById('userType').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+
+});
